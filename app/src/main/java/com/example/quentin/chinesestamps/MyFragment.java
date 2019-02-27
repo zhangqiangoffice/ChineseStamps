262edf06f77a520a913ff41c5fa4dc68d3002f9c
Quentin Zhang <quentinzhang@zillionsource.com>
2019-02-27 16:15:56 +0800

    实现底部导航栏

---
+++ app/src/main/java/com/example/quentin/chinesestamps/MyFragment.java
@@ -0,0 +1,27 @@
+package com.example.quentin.chinesestamps;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MyFragment extends Fragment {
+
+    private String content;
+
+    public MyFragment() { }
+
+    public MyFragment(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fg_content, container, false);
+        TextView txt_content = view.findViewById(R.id.txt_content);
+        txt_content.setText(content);
+        return view;
+    }
+}
